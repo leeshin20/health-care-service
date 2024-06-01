3ed7d038563fc4abb3221e5031ee1db9eebcf3b5
fbwoduf <fbwoduf2@gmail.com>
2024-06-01 18:56:43 +0900

    수정,삭제
    수정,삭제,로그아웃

--- app/src/main/java/com/example/healthcareapplication/fragments/OptionFragment.kt
+++ app/src/main/java/com/example/healthcareapplication/fragments/OptionFragment.kt
@@ -1,25 +1,40 @@
 package com.example.healthcareapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.example.healthcareapplication.DATA.ContentData
 import com.example.healthcareapplication.R
+import com.example.healthcareapplication.aboutlog.LoadingActivity
 import com.example.healthcareapplication.databinding.FragmentOptionBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class OptionFragment : Fragment() {
     private lateinit var binding: FragmentOptionBinding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_option, container, false)
         val view = binding.root
+
+        binding.signout.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(context, "로그아웃", Toast.LENGTH_SHORT).show()
+            val intent = Intent(requireContext(), LoadingActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
 
         binding.Ohometap.setOnClickListener {
             it.findNavController().navigate(R.id.action_optionFragment_to_homeFragment)
--- app/src/main/java/com/example/healthcareapplication/fragments/contentclickFragment.kt
+++ app/src/main/java/com/example/healthcareapplication/fragments/contentclickFragment.kt
@@ -1,48 +1,88 @@
 package com.example.healthcareapplication.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.healthcareapplication.DATA.ContentData
 import com.example.healthcareapplication.DATA.ContentModel
 import com.example.healthcareapplication.R
 import com.example.healthcareapplication.databinding.FragmentContentclickBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class contentclickFragment : Fragment() {
     private lateinit var binding: FragmentContentclickBinding
-
+    private lateinit var key: String
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_contentclick, container, false)
         val view = binding.root
 
-//        val title = arguments?.getString("title")
-//        val content = arguments?.getString("content")
-//        val uid = arguments?.getString("uid")
-//        val time = arguments?.getString("time")
-//
-//        binding.contenttitle.text = title
-//        binding.contentcontent.text = content
-//        binding.contenttime.text = time
-
-        val key = arguments?.getString("key")
+        binding.contentset.setOnClickListener {
+            showDialog()
+        }
+        key = arguments?.getString("key").toString()
+        getImageData(key.toString())
         Toast.makeText(requireContext(), key, Toast.LENGTH_LONG).show()
         getBoardData(key.toString())
         return view
     }
 
-    private fun getBoardData(key : String){
+    private fun showDialog(){
+        val mDialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog, null)
+        val mBuilder = AlertDialog.Builder(context)
+            .setView(mDialogView)
+            .setTitle("게시글 수정/삭제")
 
+
+        val alertDialog = mBuilder.show()
+        alertDialog.findViewById<Button>(R.id.editBtn)?.setOnClickListener {
+            val bundle = bundleOf("contentment" to key)
+            findNavController().navigate(R.id.action_contentclickFragment_to_boardEditFragment, bundle)
+            alertDialog.dismiss()
+        }
+        alertDialog.findViewById<Button>(R.id.deleteBtn)?.setOnClickListener {
+            ContentData.boardRef.child(key).removeValue()
+            Toast.makeText(requireContext(), "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_contentclickFragment_to_communityFragment)
+            alertDialog.dismiss()
+        }
+    }
+    private fun getImageData(key :String){
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+        val imageViewFF = binding.getImage
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task->
+            if(task.isSuccessful){
+                Glide.with(this).load(task.result).into(imageViewFF)
+            } else{
+            }
+        })
+
+    }
+    private fun getBoardData(key : String){
+        val userId = auth.currentUser?.uid ?: return
         val postListener = object : ValueEventListener {
             override fun onDataChange(datasnapshot: DataSnapshot) {
 
@@ -52,6 +92,14 @@ class contentclickFragment : Fragment() {
                     binding.contenttitle.text = item.title
                     binding.contentcontent.text = item.content
                     binding.contenttime.text = item.time
+
+                    val writeUid = item.uid
+                    Log.d("auth", auth.toString())
+                    if(userId == writeUid){
+                        binding.contentset.isVisible = true
+                    } else{
+
+                    }
                 }
 
                 }
